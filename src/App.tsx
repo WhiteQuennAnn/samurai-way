9fa3118534c02a177bd34e3cdcc23b5de1b13aad
WhiteQueenAnn <vronskaya.anyuta@yandex.ru>
2022-11-01 12:31:30 +0300

    01112022

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,14 @@ import './App.css';
 
 function App() {
   return (
+
     <div className="App">
-     {/*Hello, samurai! Let's go!*/}
+    <ul>
+        <li>css</li>
+        <li>html</li>
+        <li>js</li>
+        <li>react</li>
+    </ul>
     </div>
   );
 }
