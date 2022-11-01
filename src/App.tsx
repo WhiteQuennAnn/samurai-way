475cd2296a6991942e685a731a0db2dba5664001
WhiteQueenAnn <vronskaya.anyuta@yandex.ru>
2022-11-01 14:09:56 +0300

    01112022

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,19 @@ import React from 'react';
 import './App.css';
 
 const App = () => {
-  return (
-
-    <div className="App">
-    <ul>
-        <li>css</li>
-        <li>html</li>
-        <li>js</li>
-        <li>react</li>
-    </ul>
-    </div>
-  );
+    return (
+        <div>
+            <div> one div</div>
+            <div className="App">
+                <ul>
+                    <li>css</li>
+                    <li>html</li>
+                    <li>js</li>
+                    <li>react</li>
+                </ul>
+            </div>
+        </div>
+    );
 }
 
 export default App;
