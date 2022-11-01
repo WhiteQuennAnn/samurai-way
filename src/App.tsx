00207b647be1ad3b022d83663bdb755184eebae4
WhiteQueenAnn <vronskaya.anyuta@yandex.ru>
2022-11-01 15:26:02 +0300

    01112022

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
-
+import  Header from './Header';
+import  Technologies from './Technologies';
 const App = () => {
     return (
         <div>
-            <Header/>
-            < Technologies/>
+            <Header />
+            <Technologies />
         </div>
     );
 }
