2e3c52bfa2c83fb607529ea33fe702406046304d
WhiteQueenAnn <vronskaya.anyuta@yandex.ru>
2022-11-02 20:56:27 +0300

    02112022

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
-import Header from './Header';
+// import Header from './Header';
 import Technologies from './Technologies';
 
 const App = () => {
     return (
         <div>
-            <Header/>
+            {/*<Header/>*/}
             <Technologies/>
         </div>
     );
--- src/Header.tsx
+++ src/Header.tsx
@@ -3,9 +3,9 @@ const Header = () => {
     return (
 
         <div>
-            <a href="#s">Home</a>
-            <a href="#s">New Feed</a>
-            <a href="#s">Messages</a>
+            <a href="#s">Home</a> -
+            <a href="#s">New Feed</a> -
+            <a href="#s">Messages</a> -
         </div>
     );
 }
