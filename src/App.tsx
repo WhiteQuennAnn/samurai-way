18882a5540e751c512ed12a9d72ef945e0847ab3
WhiteQueenAnn <vronskaya.anyuta@yandex.ru>
2022-11-04 19:49:48 +0300

    04112022

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,9 @@ export const App = () => {
                 <div><a>Settings</a></div>
 
             </nav>
-            <div className="content">Main content</div>
+            <div className="content">
+               <img  src="https://mobimg.b-cdn.net/v3/fetch/10/10b532bc3da4dd2e0715ea146abd32d4.jpeg"/>
+            </div>
         </div>
     );
 }
