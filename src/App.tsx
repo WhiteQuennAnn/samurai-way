2c1b8661027524414f3a3633a858677309748ef2
WhiteQueenAnn <vronskaya.anyuta@yandex.ru>
2022-11-04 20:19:47 +0300

    04112022

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
 import React from 'react';
 import './App.css';
+import {Header} from "./components/header";
+import {Nav} from "./components/nav";
 
 export const App = () => {
     return (
         <div className="app-wrapper">
-            <header className="header">
-                <img
-                    src=" https://assets.turbologo.ru/blog/ru/2020/11/18161757/%D0%91%D0%B5%D0%B7-%D0%B8%D0%BC%D0%B5%D0%BD%D0%B8-1.png"/>
-            </header>
-            <nav className="nav">
-                <div><a>Profile</a></div>
-                <div><a>Messages</a></div>
-                <div><a>News</a></div>
-                <div><a>Music</a></div>
-                <div><a>Settings</a></div>
-
-            </nav>
+            <Header/>
+            <Nav/>
             <div className="content">
                 <div>
-
                     <img src="https://mobimg.b-cdn.net/v3/fetch/10/10b532bc3da4dd2e0715ea146abd32d4.jpeg"/>
                 </div>
                 <div>
