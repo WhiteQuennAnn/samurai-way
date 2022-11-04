808d163bdf2afeee2d69538833cc4a3640c06def
WhiteQueenAnn <vronskaya.anyuta@yandex.ru>
2022-11-04 19:19:13 +0300

    04112022

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import './App.css';
 export const App = () => {
     return (
         <div>
-
+<header> <img src =" https://www.google.com/url?sa=i&url=https%3A%2F%2Fpoint.bangbangeducation.ru%2Flists%2Faydentika-i-dizayn-logotipa&psig=AOvVaw1j9n46Ae54GA3jyMBz38Ag&ust=1667665087415000&source=images&cd=vfe&ved=0CA0QjRxqFwoTCOjUxtr2lPsCFQAAAAAdAAAAABAE" /> </header>
         </div>
     );
 }
