89ade6b854c2e136cefe9593718523f3faa75129
WhiteQueenAnn <vronskaya.anyuta@yandex.ru>
2022-11-04 19:46:53 +0300

    04112022

--- src/App.tsx
+++ src/App.tsx
@@ -9,13 +9,12 @@ export const App = () => {
                     src=" https://assets.turbologo.ru/blog/ru/2020/11/18161757/%D0%91%D0%B5%D0%B7-%D0%B8%D0%BC%D0%B5%D0%BD%D0%B8-1.png"/>
             </header>
             <nav className="nav">
-                <div><a>Profile</a> </div>
-                <div><a>Profile</a> </div>
-                <div><a>Profile</a> </div>
-                <div><a>Profile</a> </div>
-                <div><a>Profile</a> </div>
-                <a>Profile</a>
-                <a>Messages</a>
+                <div><a>Profile</a></div>
+                <div><a>Messages</a></div>
+                <div><a>News</a></div>
+                <div><a>Music</a></div>
+                <div><a>Settings</a></div>
+
             </nav>
             <div className="content">Main content</div>
         </div>
