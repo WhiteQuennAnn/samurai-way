1852090cdd32c29ef554fd0aa6bd9e66b6a99dec
WhiteQueenAnn <vronskaya.anyuta@yandex.ru>
2022-11-04 22:22:10 +0300

    04112022

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -1,16 +1,15 @@
 import React from 'react';
-import "./nav.css";
+import s from "./nav.module.css";
 
 export const Nav = () => {
     return (
         <div>
-
-            <nav className="nav">
-                <div className="item"><a>Profile</a></div>
-                <div className="item"><a>Messages</a></div>
-                <div className="item"><a>News</a></div>
-                <div className="item"><a>Music</a></div>
-                <div className="item"><a>Settings</a></div>
+            <nav className={s.nav}>
+                <div className={s.item}><a>Profile</a></div>
+                <div className={s.item}><a>Messages</a></div>
+                <div className={s.item}><a>News</a></div>
+                <div className={s.item}><a>Music</a></div>
+                <div className={s.item}><a>Settings</a></div>
 
             </nav>
         </div>
