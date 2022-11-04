d83d3d2318c071e65627aaf471bcc4f2e7207594
WhiteQueenAnn <vronskaya.anyuta@yandex.ru>
2022-11-04 19:24:03 +0300

    04112022

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,15 @@ import './App.css';
 export const App = () => {
     return (
         <div>
-<header> <img src =" https://www.google.com/url?sa=i&url=https%3A%2F%2Fpoint.bangbangeducation.ru%2Flists%2Faydentika-i-dizayn-logotipa&psig=AOvVaw1j9n46Ae54GA3jyMBz38Ag&ust=1667665087415000&source=images&cd=vfe&ved=0CA0QjRxqFwoTCOjUxtr2lPsCFQAAAAAdAAAAABAE" /> </header>
+            <header>
+                <img
+                    src=" https://assets.turbologo.ru/blog/ru/2020/11/18161757/%D0%91%D0%B5%D0%B7-%D0%B8%D0%BC%D0%B5%D0%BD%D0%B8-1.png"/>
+            </header>
+            <nav>
+                <div>Profile</div>
+                <div>Messages</div>
+                <div>Main content</div>
+            </nav>
         </div>
     );
 }
