dfed3f59376a6833cd3bf9b5cbc780aad7481261
WhiteQueenAnn <vronskaya.anyuta@yandex.ru>
2022-11-04 19:43:33 +0300

    04112022

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,16 @@ import './App.css';
 
 export const App = () => {
     return (
-        <div className= "app-wrapper">
-            <header className= "header">
+        <div className="app-wrapper">
+            <header className="header">
                 <img
                     src=" https://assets.turbologo.ru/blog/ru/2020/11/18161757/%D0%91%D0%B5%D0%B7-%D0%B8%D0%BC%D0%B5%D0%BD%D0%B8-1.png"/>
             </header>
-            <nav className= "nav">
+            <nav className="nav">
                 <div>Profile</div>
                 <div>Messages</div>
-                <div className= "content">Main content</div>
             </nav>
+            <div className="content">Main content</div>
         </div>
     );
 }
