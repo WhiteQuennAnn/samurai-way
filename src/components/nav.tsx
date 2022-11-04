36a3c53b2251b4a7472730d476e11ac0cad3addb
WhiteQueenAnn <vronskaya.anyuta@yandex.ru>
2022-11-04 22:39:49 +0300

    04112022

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import s from "./nav.module.css";
 
-let classesNew = `${s.item} ${s.active}`
+// let classesNew = `${s.item} ${s.active}`
 export const Nav = () => {
     return (
         <div>
             <nav className={s.nav}>
-                <div className={s.item}><a>Profile</a></div>
+                <div className={`${s.item} ${s.active}`}><a>Profile</a></div>
                 <div className={s.item}><a>Messages</a></div>
                 <div className={s.item}><a>News</a></div>
                 <div className={s.item}><a>Music</a></div>
