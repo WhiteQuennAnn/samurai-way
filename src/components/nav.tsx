83f0e105f035cf2892c44ce825d78c9ec0f1586e
WhiteQueenAnn <vronskaya.anyuta@yandex.ru>
2022-11-04 21:27:37 +0300

    04112022

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -5,11 +5,11 @@ export const Nav = () => {
         <div>
 
             <nav className="nav">
-                <div><a>Profile</a></div>
-                <div><a>Messages</a></div>
-                <div><a>News</a></div>
-                <div><a>Music</a></div>
-                <div><a>Settings</a></div>
+                <div className="item"><a>Profile</a></div>
+                <div className="item"><a>Messages</a></div>
+                <div className="item"><a>News</a></div>
+                <div className="item"><a>Music</a></div>
+                <div className="item"><a>Settings</a></div>
 
             </nav>
         </div>
