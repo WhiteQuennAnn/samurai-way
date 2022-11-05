de9bc48d1e7ba4a3c54bbd3224aba73197657ed4
WhiteQueenAnn <vronskaya.anyuta@yandex.ru>
2022-11-05 17:28:11 +0300

    05112022

---
+++ src/components/Profile/My posts/Post/Post.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import s from "./Post.module.css";
+
+export const Post = () => {
+    return (
+        <div className={s.item}>
+            <img src={"https://klike.net/uploads/posts/2022-06/1655792831_4.jpg"}/>
+            post 1
+        </div>
+    )
+}
