4e7d1f18e7c13e6ad403876b40c645088215e542
WhiteQueenAnn <vronskaya.anyuta@yandex.ru>
2022-11-05 18:01:12 +0300

    05112022

--- src/components/Profile/My posts/Post/Post.tsx
+++ src/components/Profile/My posts/Post/Post.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import s from "./Post.module.css";
 
-export const Post = (props) => {
-    props.message
+export const Post = (props: any) => {
+
     return (
         <div className={s.item}>
             <img src={"https://klike.net/uploads/posts/2022-06/1655792831_4.jpg"}/>
-            post 1
+            {props.message}
             <div>
                 <span>like</span>
             </div>
