e10b54ff0fa6b5a5839b5f7c01f010273697f65b
WhiteQueenAnn <vronskaya.anyuta@yandex.ru>
2022-11-05 17:30:11 +0300

    05112022

--- src/components/Profile/My posts/MyPosts.tsx
+++ src/components/Profile/My posts/MyPosts.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import s from "./MyPosts.module.css";
+import {Post} from "./Post/Post";
 
 export const MyPosts = () => {
     return (
@@ -10,23 +11,12 @@ export const MyPosts = () => {
                     <button> Add post </button>
                     <button> Remove </button>
                 </div>
+
                 <div className={s.posts}>
-                    <div className={s.item}>
-                        <img src={"https://klike.net/uploads/posts/2022-06/1655792831_4.jpg"}/>
-                        post 1
-                    </div>
-                    <div className={s.item}>
-                        post 2
-                    </div>
-                    <div className={s.item}>
-                        post 3
-                    </div>
-                    <div className={s.item}>
-                        post 4
-                    </div>
-                    <div className={s.item}>
-                        post 5
-                    </div>
+                   <Post /> <Post />
+                   <Post /> <Post />
+                   <Post /> <Post />
+                   <Post /> <Post />
                 </div>
             </div>
            )
