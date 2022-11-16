ae060432a4e6a193e88c462e4dec9ba01c7f87cd
WhiteQueenAnn <vronskaya.anyuta@yandex.ru>
2022-11-16 16:37:28 +0300

    16112022

--- src/components/Nav/nav.tsx
+++ src/components/Nav/nav.tsx
@@ -6,8 +6,8 @@ export const Nav = () => {
     return (
         <div>
             <nav className={s.nav}>
-                <div className={`${s.item} ${s.active}`}><a>Profile</a></div>
-                <div className={s.item}><a>Messages</a></div>
+                <div className={`${s.item} ${s.active}`}><a href="/profile">Profile</a></div>
+                <div className={s.item}><a href="/dialogs">Messages</a></div>
                 <div className={s.item}><a>News</a></div>
                 <div className={s.item}><a>Music</a></div>
                 <div className={s.item}><a>Settings</a></div>
