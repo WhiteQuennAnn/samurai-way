f5936552430bcd0852b1035e812cfbc9e72264f3
WhiteQueenAnn <vronskaya.anyuta@yandex.ru>
2022-11-16 16:18:17 +0300

    16112022

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,18 @@ import './App.css';
 import {Header} from "./components/Header/header";
 import {Nav} from "./components/Nav/nav";
 import {Profile} from "./components/Profile/profile";
+import {Dialogs} from "./components/Dialogs/Dialogs";
 
 export const App = () => {
     return (
         <div className="app-wrapper">
             <Header/>
             <Nav/>
-            <Profile/>
+            {/*<Profile/>*/}
+            <div className=" app-wrapper-content">
+                <Dialogs/>
+            </div>
+
         </div>
     );
 }
---
+++ src/components/Dialogs/Dialogs.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import s from './Dialogs.module.css';
+
+export const Dialogs = (props: any) => {
+    return (
+        <div className={s.header}>
+            Dialogs
+        </div>
+    );
+};
+
