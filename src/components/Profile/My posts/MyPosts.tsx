c973122ce13da3314fa7dac6799cafb289cb1edd
WhiteQueenAnn <vronskaya.anyuta@yandex.ru>
2022-11-22 20:06:54 +0300

    22112022

--- src/components/Profile/My posts/MyPosts.tsx
+++ src/components/Profile/My posts/MyPosts.tsx
@@ -4,9 +4,11 @@ import {Post} from "./Post/Post";
 
 export const MyPosts = () => {
     let postsData = [
-        {id: 1, message: "Hey how are you", likesCount: 22 },
+        {id: 1, message: "Hey how are you", likesCount: 22},
         {id: 2, message: "its my first post", likesCount: 65}
     ]
+    let postsElements = postsData
+        .map(p => <Post message={p.message} likesCount={p.likesCount}/>)
     return (
         <div className={s.postsBlock}>
             <h3> My posts</h3>
@@ -18,10 +20,7 @@ export const MyPosts = () => {
             </div>
 
             <div className={s.posts}>
-                <Post message={postsData[0].message} likesCount={postsData[0].likesCount}/>
-                <Post message={postsData[1].message} likesCount={postsData[1].likesCount}/>
-
-
+                {postsElements}
             </div>
         </div>
     )
