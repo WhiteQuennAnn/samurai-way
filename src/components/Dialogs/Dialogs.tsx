d4f180593dbc2d0404992b4c53210246392fdc0d
WhiteQueenAnn <vronskaya.anyuta@yandex.ru>
2022-11-22 19:18:41 +0300

    22112022

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -27,10 +27,16 @@ const MessageItem = (props: MessageItemProps) => {
 
 export const Dialogs = (props: any) => {
 
+
     let dialogsData = [
         {id: 1, name: "Anya"},
         {id: 2, name: "Dima"},
         {id: 3, name: "Igor"}
+    ]
+    let dialogsElements =[
+        <DialogItem name={ dialogsData[0].name} id={dialogsData[0].id}/>,
+        <DialogItem name={dialogsData[1].name} id={dialogsData[1].id}/>,
+        <DialogItem name={dialogsData[2].name} id={dialogsData[2].id}/>
     ]
     let messageItemData = [
         {id: 1, message: "Hello"},
@@ -42,15 +48,18 @@ export const Dialogs = (props: any) => {
         <div className={s.dialogs}>
 
             <div className={s.dialogsItems}>
-                <DialogItem name={ dialogsData[0].name} id={dialogsData[0].id}/>
-                <DialogItem name={dialogsData[1].name} id={dialogsData[1].id}/>
-                <DialogItem name={dialogsData[2].name} id={dialogsData[2].id}/>
+
+                {dialogsElements}
+
+                {/*<DialogItem name={ dialogsData[0].name} id={dialogsData[0].id}/>*/}
+                {/*<DialogItem name={dialogsData[1].name} id={dialogsData[1].id}/>*/}
+                {/*<DialogItem name={dialogsData[2].name} id={dialogsData[2].id}/>*/}
 
             </div>
             <div className={s.messages}>
-                <MessageItem message={messageItemData[0].message}/>
-                <MessageItem message={messageItemData[1].message}/>
-                <MessageItem message={messageItemData[2].message}/>
+                {/*<MessageItem message={messageItemData[0].message}/>*/}
+                {/*<MessageItem message={messageItemData[1].message}/>*/}
+                {/*<MessageItem message={messageItemData[2].message}/>*/}
 
             </div>
 
