3ff0ef62a5fe710071ebc45ab1d323eb68de6eae
WhiteQueenAnn <vronskaya.anyuta@yandex.ru>
2022-11-23 23:04:49 +0300

    23112022

--- src/index.tsx
+++ src/index.tsx
@@ -2,22 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import {App} from './App';
+import state from './redux/state';
 
-let dialogsData = [
-    {id: 1, name: "Anya"},
-    {id: 2, name: "Dima"},
-    {id: 3, name: "Igor"}
-]
-
-let messageItemData = [
-    {id: 1, message: "Hello"},
-    {id: 2, message: "Pike"},
-    {id: 3, message: "Buy"}
-]
-
-let postsData = [
-    {id: 1, message: "Hey how are you", likesCount: 22},
-    {id: 2, message: "its my first post", likesCount: 65}
+// let dialogsData = [
+//     {id: 1, name: "Anya"},
+//     {id: 2, name: "Dima"},
+//     {id: 3, name: "Igor"}
+// ]
+//
+// let messageItemData = [
+//     {id: 1, message: "Hello"},
+//     {id: 2, message: "Pike"},
+//     {id: 3, message: "Buy"}
+// ]
+//
+// let postsData = [
+//     {id: 1, message: "Hey how are you", likesCount: 22},
+//     {id: 2, message: "its my first post", likesCount: 65}
 ]
 ReactDOM.render(
     <App postsData={postsData} dialogsData={dialogsData} messageItemData={messageItemData}/>,
---
+++ src/redux/state.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+let state = {
+    dialogsData: [
+        {id: 1, name: "Anya"},
+        {id: 2, name: "Dima"},
+        {id: 3, name: "Igor"}
+    ],
+
+    messageItemData: [
+        {id: 1, message: "Hello"},
+        {id: 2, message: "Pike"},
+        {id: 3, message: "Buy"}
+    ],
+    postsData: [
+        {id: 1, message: "Hey how are you", likesCount: 22},
+        {id: 2, message: "its my first post", likesCount: 65}]
+}
+export default state;
