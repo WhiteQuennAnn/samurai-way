74e721a386fdb8462dade11447947f254a3e57be
WhiteQueenAnn <vronskaya.anyuta@yandex.ru>
2022-11-30 14:46:28 +0300

    31 commit

--- src/components/Profile/My posts/MyPosts.tsx
+++ src/components/Profile/My posts/MyPosts.tsx
@@ -22,17 +22,20 @@ export const MyPosts = (props: MyPostsTypeProps) => {
 
     let postsElements = props.postsData.map(p => <Post message={p.message} likesCount={p.likesCount}/>)
 
-    let newPostElement = React.createRef();
+    let newPostElement = React.createRef<HTMLTextAreaElement>();
 
     let addPost = () => {
-        alert('heewefwvesdvbesbv')
+        if (newPostElement.current !== null) {
+            let text = newPostElement.current.value;
+            alert(text)
+        }
     }
 
     return (
         <div className={s.postsBlock}>
             <h3> My posts</h3>
             <div>
-                <div><textarea></textarea></div>
+                <div><textarea ref={newPostElement}></textarea></div>
                 <div>
                     <button onClick={addPost}> Add post
                     </button>
