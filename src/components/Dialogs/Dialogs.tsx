b4e50357de90a95296ddf9b63b719c904895e947
WhiteQueenAnn <vronskaya.anyuta@yandex.ru>
2022-12-04 12:26:04 +0300

    33 commit

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -4,39 +4,12 @@ import {DialogItem} from "./DialogItem/DialogsItem";
 import {MessageItem} from "./Message/Message";
 import {Global} from "../../Type";
 
-// export type Global = {
-//     dialogsData: Array<OneDialogsDataPropsType>
-//     messageItemData: Array<OneMessageItemDataProps>
-// }
-// export type OneDialogsDataPropsType = {
-//     id: number
-//     name: string
-// }
-//
-// export type OneMessageItemDataProps = {
-//     id: number
-//     message: string
-// }
-
-
 export const Dialogs = (props: Global) => {
-
-    // let dialogsData = [
-    //     {id: 1, name: "Anya"},
-    //     {id: 2, name: "Dima"},
-    //     {id: 3, name: "Igor"}
-    // ]
-    //
-    // let messageItemData = [
-    //     {id: 1, message: "Hello"},
-    //     {id: 2, message: "Pike"},
-    //     {id: 3, message: "Buy"}
-    // ]
 
     let dialogsElements = props.dialogsPage.dialogsData
         .map(d => <DialogItem name={d.name} id={d.id}/>);
 
-    let messagesElenents = props. dialogsPage.messageItemData
+    let messagesElenents = props.dialogsPage.messageItemData
         .map(m => <MessageItem message={m.message}/>)
     return (
 
