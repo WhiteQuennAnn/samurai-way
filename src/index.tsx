9e1546518acd4a4b0de4144f98fd359aaf05b396
WhiteQueenAnn <vronskaya.anyuta@yandex.ru>
2022-12-10 21:50:28 +0300

    35 commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,16 @@
 import React from 'react';
 import './index.css';
-import {rerenderEntireTree} from "./render";
-import state from "./redux/state";
+import state, {addPost, updateNewPostText} from "./redux/state";
 
+import {StateType} from "./Type";
+import ReactDOM from "react-dom";
+import {App} from "./App";
+export let rerenderEntireTree = (state: StateType) => {
+    ReactDOM.render(
+        <App state={state} addPost={addPost} newPostText={"newPostText"} updateNewPostText={ updateNewPostText}/>,
+        document.getElementById('root')
+    );
+}
 rerenderEntireTree(state);
 
 
