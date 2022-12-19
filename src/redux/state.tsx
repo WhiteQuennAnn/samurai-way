2ed22731f7f4c75715e61d3080772d13da5a0adb
WhiteQueenAnn <vronskaya.anyuta@yandex.ru>
2022-12-19 18:43:03 +0300

    37 commit

--- src/redux/state.tsx
+++ src/redux/state.tsx
@@ -24,52 +24,26 @@ let store = {
     },
     rerenderEntireTree() {
         console.log("State is changed")
-    }
-}
-
-let rerenderEntireTree = () => {
-    console.log("State is changed")
-}
-
-let state = {
-    profilePage: {
-        postsData: [
-            {id: 1, message: "Hey how are you", likesCount: 22},
-            {id: 2, message: "its my first post", likesCount: 65}],
-        newPostText: "Anna"
     },
-    dialogsPage: {
-        messageItemData: [
-            {id: 1, message: "Hello"},
-            {id: 2, message: "Pike"},
-            {id: 3, message: "Buy"}
-        ],
-        dialogsData: [
-            {id: 1, name: "Anya"},
-            {id: 2, name: "Dima"},
-            {id: 3, name: "Igor"}
-        ],
+    addPost(postMessage: string) {
+        const newPost: OnePostTypeProps = {
+            id: new Date().getTime(),
+            message: postMessage,
+            likesCount: 94
+        };
+        state.profilePage.postsData.push(newPost)
+        state.profilePage.newPostText = ' '
+        rerenderEntireTree();
+    },
+    updateNewPostText(newText: string) {
+        state.profilePage.newPostText = newText;
+        rerenderEntireTree();
+    },
+    subscribe(observer: () => void) {
+        rerenderEntireTree = observer; // наблюдатель
     }
 }
 
-export const addPost = (postMessage: string) => {
-    const newPost: OnePostTypeProps = {
-        id: new Date().getTime(),
-        message: postMessage,
-        likesCount: 94
-    };
-    state.profilePage.postsData.push(newPost)
-    state.profilePage.newPostText = ' '
-    rerenderEntireTree();
-}
 
-export const updateNewPostText = (newText: string) => {
-    state.profilePage.newPostText = newText;
-    rerenderEntireTree();
-}
-
-export const subscribe = (observer: () => void) => {
-    rerenderEntireTree = observer; // наблюдатель
-}
-
-export default state;
+export default store;
+// window.store = store;
