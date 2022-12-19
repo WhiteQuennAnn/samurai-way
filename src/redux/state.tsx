2a370d58bc1695dd4fd26a8aceea9db14fdbfbbe
WhiteQueenAnn <vronskaya.anyuta@yandex.ru>
2022-12-19 18:38:21 +0300

    37 commit

--- src/redux/state.tsx
+++ src/redux/state.tsx
@@ -1,6 +1,32 @@
 import React from 'react';
 import {OnePostTypeProps} from "../Type";
 
+let store = {
+    _state: {
+        profilePage: {
+            postsData: [
+                {id: 1, message: "Hey how are you", likesCount: 22},
+                {id: 2, message: "its my first post", likesCount: 65}],
+            newPostText: "Anna"
+        },
+        dialogsPage: {
+            messageItemData: [
+                {id: 1, message: "Hello"},
+                {id: 2, message: "Pike"},
+                {id: 3, message: "Buy"}
+            ],
+            dialogsData: [
+                {id: 1, name: "Anya"},
+                {id: 2, name: "Dima"},
+                {id: 3, name: "Igor"}
+            ],
+        }
+    },
+    rerenderEntireTree() {
+        console.log("State is changed")
+    }
+}
+
 let rerenderEntireTree = () => {
     console.log("State is changed")
 }
