75219fa04edc134e7cbe6f5c21cc6435747622c4
WhiteQueenAnn <vronskaya.anyuta@yandex.ru>
2022-12-23 23:11:50 +0300

    38 commit

--- src/components/Nav/nav.tsx
+++ src/components/Nav/nav.tsx
@@ -5,18 +5,12 @@ import {NavLink} from "react-router-dom";
 // let classesNew = `${s.item} ${s.active}`
 export const Nav = () => {
     return (
-        <div>
-            <nav className={s.nav}>
-                <div className={`${s.item} ${s.active}`}>
-                    <NavLink to="/profile" activeClassName={s.activeLink}>Profile</NavLink></div>
-                <div className={`${s.item} ${s.active}`}>
-                    <NavLink to="/dialogs" activeClassName={s.activeLink}>Messages</NavLink></div>
-                <div className={s.item}><a>News</a></div>
-                <div className={s.item}><a>Music</a></div>
-                <div className={s.item}><a>Settings</a></div>
-
-            </nav>
-        </div>
+        <nav className={s.nav}>
+            <div className={s.item}><NavLink to='/profile' activeClassName={s.active}>Profile</NavLink></div>
+            <div className={s.item}><NavLink to='/dialogs' activeClassName={s.active}>Messages</NavLink></div>
+            <div className={s.item}><NavLink to='/news' activeClassName={s.active}>News</NavLink></div>
+            <div className={s.item}><NavLink to='/music' activeClassName={s.active}>Music</NavLink></div>
+            <div className={s.item}><NavLink to='/setting' activeClassName={s.active}>Settings</NavLink></div>
+        </nav>
     )
-
 }
