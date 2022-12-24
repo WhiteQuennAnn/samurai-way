90ac56fc58ebe68139b1b0bf8fea55eb5b00e4a0
WhiteQueenAnn <vronskaya.anyuta@yandex.ru>
2022-12-24 03:53:41 +0300

    40 commit

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, {ChangeEvent} from 'react';
 import s from './Dialogs.module.css';
 import {DialogItem} from "./DialogItem/DialogsItem";
 import {MessageItem} from "./Message/Message";
 import {Global} from "../../Type";
+import {updateNewMessageBodyCreator} from "../../redux/state";
 
 export const Dialogs = (props: Global) => {
 
@@ -18,8 +19,9 @@ export const Dialogs = (props: Global) => {
     let onSendMessageClick = () => {
 
     }
-    let onNewMessageChange = () => {
-
+    let onNewMessageChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
+        let body = e.target.value
+        props.store.dispatch( updateNewMessageBodyCreator(body))
     }
     return (
 
