85a166b7ffc3e636328da7b84ca3a8de7853ede4
WhiteQueenAnn <vronskaya.anyuta@yandex.ru>
2022-12-25 18:11:45 +0300

    41 commit

--- src/redux/DialogsReducer.tsx
+++ src/redux/DialogsReducer.tsx
@@ -1,13 +1,11 @@
 const DialogsReducer = (state, action) => {
 
     if (action.type === 'UPDATE-NEW-MESSAGE-BODY') {
-        this._state.dialogsPage.newMessageBody = action.body;
-        this._onChange();
+        state.newMessageBody = action.body;
     } else if (action.type === 'SEND-MESSAGE') {
-        let body = this._state.dialogsPage.newMessageBody;
-        this._state.dialogsPage.newMessageBody = "";
-        this._state.dialogsPage.messageItemData.push({id: 4, message: body})
-        this._onChange();
+        let body = state.newMessageBody;
+        state.newMessageBody = "";
+        state.messageItemData.push({id: 4, message: body})
     }
 
     return state;
