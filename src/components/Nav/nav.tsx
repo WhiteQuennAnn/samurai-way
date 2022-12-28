39473523cdcce0a75f57efae5172dcd6e4aa05de
WhiteQueenAnn <vronskaya.anyuta@yandex.ru>
2022-12-28 20:03:04 +0300

    42 commit

--- src/components/Nav/nav.tsx
+++ src/components/Nav/nav.tsx
@@ -11,6 +11,23 @@ export const Nav = () => {
             <div className={s.item}><NavLink to='/news' activeClassName={s.active}>News</NavLink></div>
             <div className={s.item}><NavLink to='/music' activeClassName={s.active}>Music</NavLink></div>
             <div className={s.item}><NavLink to='/setting' activeClassName={s.active}>Settings</NavLink></div>
+            {/*<ul>*/}
+            {/*    <li style="--clr:#00ade1">*/}
+            {/*        <a href="#" data-text="&nbsp;Home">&nbsp;Home&nbsp;</a>*/}
+            {/*    </li>*/}
+            {/*    <li style="--clr:#ff6492">*/}
+            {/*        <a href="#" data-text="&nbsp;About">&nbsp;About&nbsp;</a>*/}
+            {/*    </li>*/}
+            {/*    <li style="--clr:#ffdd1c">*/}
+            {/*        <a href="#" data-text="&nbsp;Services">&nbsp;Services&nbsp;</a>*/}
+            {/*    </li>*/}
+            {/*    <li style="--clr:#00dc82">*/}
+            {/*        <a href="#" data-text="&nbsp;Blog">&nbsp;Blog&nbsp;</a>*/}
+            {/*    </li>*/}
+            {/*    <li style="--clr:#dc00d4">*/}
+            {/*        <a href="#" data-text="&nbsp;Contact">&nbsp;Contact&nbsp;</a>*/}
+            {/*    </li>*/}
+            {/*</ul>*/}
         </nav>
     )
 }
