f52afb1643912c3b0e6336c5f050c093f02c3235
WhiteQueenAnn <vronskaya.anyuta@yandex.ru>
2023-01-03 16:50:12 +0300

    43 commit

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import {Profile} from "./components/Profile/profile";
 import {Dialogs} from "./components/Dialogs/Dialogs";
 import {BrowserRouter, Route} from "react-router-dom";
 import {AppStoreType} from "./redux/redux-store";
+import {DialogsContainer} from "./components/Dialogs/DialogsContainer";
 
 export type PropsType = {
     store: AppStoreType
@@ -18,7 +19,7 @@ export const App: React.FC<PropsType> = (props) => {
                 <Header/>
                 <Nav/>
                 <div className="app-wrapper-content">
-                    <Route path='/dialogs' render={() => <Dialogs
+                    <Route path='/dialogs' render={() => <DialogsContainer
                         store={props.store}
                         // dialogsPage={props.store.getState().dialogsPage}
                         // dispatch={props.store.dispatch.bind(props.store)}
