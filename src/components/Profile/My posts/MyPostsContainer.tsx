b3fa773c045ba69af3fad2c45cadaf4d810e162a
WhiteQueenAnn <vronskaya.anyuta@yandex.ru>
2023-01-03 18:45:29 +0300

    44 commit

--- src/components/Profile/My posts/MyPostsContainer.tsx
+++ src/components/Profile/My posts/MyPostsContainer.tsx
@@ -1,9 +1,6 @@
 import React from 'react';
-import {OnePostTypeProps} from "../../../Type";
 import {
     addPostActionCreator,
-    AddPostActionType,
-    ChangeNewTextActionType,
     updateNewPostActionCreator
 } from "../../../redux/ProfileReducer";
 import {MyPosts} from "./MyPosts";
@@ -12,18 +9,18 @@ import StoreContext from "../../../StoreContext";
 
 export const MyPostsContainer = (props: PropsType) => {
 
-
-    let addPost = () => {
-        props.store.dispatch(addPostActionCreator())
-    }
-
-    let onPostChange = (text: string) => {
-        let action = updateNewPostActionCreator(text)
-        props.store.dispatch(action)
-    }
-
     return (
         <StoreContext.Consumer>{(store) => {
+
+            let addPost = () => {
+                props.store.dispatch(addPostActionCreator())
+            }
+
+            let onPostChange = (text: string) => {
+                let action = updateNewPostActionCreator(text)
+                props.store.dispatch(action)
+            }
+
             let state = props.store.getState()
             return <MyPosts postsData={state.profilePage.postsData}
                             newPostText={state.profilePage.newPostText}
