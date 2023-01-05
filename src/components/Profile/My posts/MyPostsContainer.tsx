14b965242de5cfa8ec55b4c4e4ae1d66232d9b22
WhiteQueenAnn <vronskaya.anyuta@yandex.ru>
2023-01-05 21:32:04 +0300

    47 commit

--- src/components/Profile/My posts/MyPostsContainer.tsx
+++ src/components/Profile/My posts/MyPostsContainer.tsx
@@ -5,13 +5,17 @@ import {
 } from "../../../redux/ProfileReducer";
 import {MyPosts} from "./MyPosts";
 import {PropsType} from "../../../App";
-
 import {connect} from "react-redux";
 import {Dialogs} from "../../Dialogs/Dialogs";
-import {Global} from "../../../Type";
+import {Global, OnePostTypeProps} from "../../../Type";
 import {AppStoreType} from "../../../redux/redux-store";
 
-const mapStateToProps = (state: AppStoreType) => {
+type mapStateToPropstype = {
+    postsData: Array<OnePostTypeProps>
+    newPostText: string
+}
+
+const mapStateToProps = (state: AppStoreType): mapStateToPropstype => {
     return {
         posts: state.profilePage.postsData,
         newPostText: state.profilePage.newPostText
