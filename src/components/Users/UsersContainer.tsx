7f7837d91c55d515e80a529ca9e923d28c33df19
WhiteQueenAnn <vronskaya.anyuta@yandex.ru>
2023-01-06 18:34:51 +0300

    49 commit

--- src/components/Users/UsersContainer.tsx
+++ src/components/Users/UsersContainer.tsx
@@ -3,19 +3,28 @@ import Users from "./Users";
 import {connect} from "react-redux";
 import {AppStoreType} from "../../redux/redux-store";
 import {Dispatch} from "redux";
+import {followAC, setUsersAC, setUsersACType, unFollowAC} from "../../redux/UsersReducer";
+import users from "./Users";
 
-let mapStateToProps =(state: AppStoreType) => {
+let mapStateToProps = (state: AppStoreType) => {
     return {
         users: state.usersPage.users
     }
 }
 
-let mapDispatchToProps =(dispatch: Dispatch) => {
+let mapDispatchToProps = (dispatch: Dispatch) => {
     return {
-
+        follow: (userId: number) => {
+            dispatch(followAC(userId))
+        },
+        unfollow: (userId: number) => {
+            dispatch(unFollowAC(userId))
+        },
+        setUsers: (users: Array<string>) => {
+            dispatch(setUsersAC(users))
+        }
     }
 }
 
 
-
-export default  connect(mapStateToProps, mapDispatchToProps) (Users);
+export default connect(mapStateToProps, mapDispatchToProps)(Users);
