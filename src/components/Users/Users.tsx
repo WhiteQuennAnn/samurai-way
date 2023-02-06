5e9069ef4812b21d2472733d3cb4ecc7140e8174
WhiteQueenAnn <vronskaya.anyuta@yandex.ru>
2023-02-06 22:09:17 +0300

    51 commit users API

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -4,32 +4,34 @@ import axios from "axios";
 
 let Users = (props: any) => {
     if (props.users.length === 0) {
-        axios.get("https://social-network.samuraijs.com/api/1.0/")
-        props.setUsers(
-            [
-                {
-                    id: 1,
-                    photoUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRIeKOvIvCR4DM345hjZsPpWCh-WISvmvM5sFalyQuU&s',
-                    followed: false,
-                    fullName: "Igor",
-                    status: 'I am boss',
-                    location: {city: 'Minsk', country: 'Belarus'}
-                },
-                {
-                    id: 2,
-                    photoUrl: 'https://strana.today/img/forall/u/0/36/foto-1-pozdravleniya-s-rozhdestvom-svoimi-slovami.jpg',
-                    followed: true,
-                    fullName: "Sasha",
-                    status: 'I am teacher',
-                    location: {city: 'Madrid', country: 'Spane'}
-                },
-                {
-                    id: 3,
-                    photoUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR1-WH5AQLiQj5kaHs7HpoVAYooOWsaJoZj1ynrETM&s ',
-                    followed: false, fullName: "Dima", status: 'I am doctor', location: {city: 'Rome', country: 'Italy'}
-                },
-            ]
-        )
+        axios.get("https://social-network.samuraijs.com/api/1.0/users").then( response=> {
+            props.setUsers(
+                [
+                    {
+                        id: 1,
+                        photoUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRIeKOvIvCR4DM345hjZsPpWCh-WISvmvM5sFalyQuU&s',
+                        followed: false,
+                        fullName: "Igor",
+                        status: 'I am boss',
+                        location: {city: 'Minsk', country: 'Belarus'}
+                    },
+                    {
+                        id: 2,
+                        photoUrl: 'https://strana.today/img/forall/u/0/36/foto-1-pozdravleniya-s-rozhdestvom-svoimi-slovami.jpg',
+                        followed: true,
+                        fullName: "Sasha",
+                        status: 'I am teacher',
+                        location: {city: 'Madrid', country: 'Spane'}
+                    },
+                    {
+                        id: 3,
+                        photoUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR1-WH5AQLiQj5kaHs7HpoVAYooOWsaJoZj1ynrETM&s ',
+                        followed: false, fullName: "Dima", status: 'I am doctor', location: {city: 'Rome', country: 'Italy'}
+                    },
+                ]
+            )
+        })
+
     }
     return (
         <div>
